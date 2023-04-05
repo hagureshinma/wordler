83cd756b815f4e4f20b360f18f3cfc3e3f1fe10a
HagureShinma <hagureshinma@gmail.com>
2023-04-05 10:20:33 -0500

    Agregados eventos de teclado a inputs.

--- js/carga.js
+++ js/carga.js
@@ -1,10 +1,12 @@
 const inputs = document.querySelectorAll(".letra");
 const candados = document.querySelectorAll(".candado"); 
 
-window.onload = function()
+inputs[0].focus();
+
+window.onload = function() 
 {
     //debería poner el focus en el primer input, pero ni mergas.
-    () => inputs[0].focus();
+    // () => inputs[0].focus(); // solucionado arriba
 
     // letras.forEach((campo)=>{campo.addEventListener('blur', (event)=>{event.target.style.background = "blue"})});
 
@@ -18,13 +20,29 @@ window.onload = function()
     //     });
     // })
 
-    inputs.forEach((input) =>{
-        input.addEventListener('keyup', () => {
-            if(input.value === ""){
-                input.style.background = "white";
-            }else{
+    inputs.forEach((input, index) =>{
+        input.addEventListener('keyup', event => {
+            if(event.key.length === 1){
                 input.style.background = "var(--w-green)";
+                if(index < inputs.length - 1){
+                    inputs[index + 1].focus();
+                }
+            }else if(event.key === 'Backspace'){
+                input.style.background = "white";
+               if(input.value === "" && index > 0){
+                inputs[index - 1].focus();
+               }
             }
         })
-    })
+    });
+
+    // inputs.forEach((input) =>{
+    //     input.addEventListener('keyup', () => {
+    //         if(input.value === ""){
+    //             input.style.background = "white";
+    //         }else{
+    //             input.style.background = "var(--w-green)";
+    //         }
+    //     })
+    // })
 }
