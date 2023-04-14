25f1725bdc768c7321ef6b9f043d3fffd99a5a96
HagureShinma <hagureshinma@gmail.com>
2023-04-14 09:56:20 -0500

    Intentando mostrar mejor las definiciones.

--- js/wordler.js
+++ js/wordler.js
@@ -2,7 +2,6 @@ const letras = document.querySelectorAll(".letra");
 const result = document.querySelector(".result");
 const definiciones = document.querySelector(".definiciones");
 const entrada = document.querySelector(".entrada");
-// const pronunciacion = document.getElementById("#pronunciacion");
 const wordlerDB = [];
 fetch("https://sheet.best/api/sheets/3760199c-7456-4774-bfae-643cb202c76d")
     .then((response) => response.json())
@@ -30,7 +29,6 @@ function filtrar(){
     letras.forEach((letra, index) =>{
         if(letra.value.length > 0){
             arreglos_usados.push([letra.value, index]);
-            //estas 2 variables existen de momento porque no sé como agarrar un solo valor de un arreglo de arreglos :p
             una_letra = letra.value;
             un_index = index;
         }
@@ -103,6 +101,7 @@ function mostrarResultado(){
     agregarClick();
 
 }
+
 function mostrarResultadoFiltro(){
     
     limpiarResultados();
@@ -117,24 +116,27 @@ function mostrarResultadoFiltro(){
 }
 
 //agrego evento click y definiciones a la lista de palabras que se creen.
-function agregarClick(){
+function agregarClick() {
     palabras = document.querySelectorAll("li");
-
+  
     palabras.forEach((palabra) => {
-        palabra.addEventListener('click', event => {
-            event.preventDefault();
-
-            obtenerDefinicion(palabra.textContent).then(definicion => {
-                entrada.textContent = palabra.textContent;
-                definiciones.textContent = definicion;
-            }).catch(error => {
-                // alert("No se encontró definición para la palabara " + palabra.textContent);
-                entrada.textContent = palabra.textContent;
-                definiciones.textContent = "No se encontró una definición para esta palabra";
-            }); 
-        })
-    })
-}
+      palabra.addEventListener("click", (event) => {
+        event.preventDefault();
+  
+        obtenerDefinicion(palabra.textContent)
+          .then((definicion) => {
+            entrada.textContent = palabra.textContent;
+            definiciones.textContent = definicion;
+          })
+          .catch((error) => {
+            entrada.textContent = palabra.textContent;
+            definiciones.textContent =
+              "No se encontró una definición para esta palabra";
+          });
+      });
+    });
+  }
+  
 
 function obtenerDefinicion(palabra){
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${palabra}`;
@@ -148,7 +150,7 @@ function obtenerDefinicion(palabra){
 
     }).then(data => {
         if(data.length === 0){
-            throw new error('No se encontró ninguna definición.');
+            throw new error('No se encontró ninguna definición.')
         }
 
         /* metodo 1 trae la primera definición del diccionario 
@@ -158,7 +160,7 @@ function obtenerDefinicion(palabra){
 
          */
 
-        /*metodo 2 trae todas las definiciones de una palabra en una cadena de texto 
+         /* metodo 2 trae todas las definiciones de una palabra en una cadena de texto */
         const definicion = data.flatMap( entry => {
             return entry.meanings.flatMap(meaning => {
                 return meaning.definitions.map(definition => {
@@ -167,60 +169,10 @@ function obtenerDefinicion(palabra){
             })
         });
 
-        return definicion.join('\n'); */
-
-        /*metodo 3 definicion presentable*/
-        const definicion = JSON.stringify(data, null, 2);
-
-        return definicion;
+        return definicion.join('\n');
 
     });
 }
 
-/*function showDefinition(event) {
-    // Get the selected word from the clicked list item
-    const word = event.target.textContent;
+
   
-    // Fetch the definition from the API
-    fetchDefinition(word)
-      .then(definition => {
-        // Create a new <article> element to display the definition
-        const article = document.createElement('article');
-  
-        // Split the definition into separate definitions
-        const definitions = definition.split('\n');
-  
-        // Create a <div> element for each definition and append it to the <article> element
-        definitions.forEach((def, index) => {
-          // Create a <div> element for the definition
-          const div = document.createElement('div');
-  
-          // Create a bolded title for the definition
-          const title = document.createElement('strong');
-          title.textContent = `Definition ${index + 1}:`;
-  
-          // Create a numbered list of examples for the definition
-          const examplesList = document.createElement('ol');
-          const examples = def.split('Examples:')[1].split(';');
-          examples.forEach(example => {
-            const listItem = document.createElement('li');
-            listItem.textContent = example.trim();
-            examplesList.appendChild(listItem);
-          });
-  
-          // Append the title and examples list to the <div> element
-          div.appendChild(title);
-          div.appendChild(examplesList);
-  
-          // Append the <div> element to the <article> element
-          article.appendChild(div);
-        });
-  
-        // Append the <article> element to the <body> element
-        document.body.appendChild(article);
-      })
-      .catch(error => {
-        console.error(error);
-        alert('Failed to fetch definition.');
-      });
-  }*/
